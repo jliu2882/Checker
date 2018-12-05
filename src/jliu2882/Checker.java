dc89c7c568a6f5551a0ac364ab0af2bc6ac5b47e
BT_1N3_32 <jliu2882@bths.edu>
2018-12-05 11:46:29 -0500

    Initial commit

---
+++ src/jliu2882/Checker.java
@@ -0,0 +1,9 @@
+package jliu2882;
+
+public interface Checker {
+    /**
+     * @param text a string to consider for acceptance
+     * @return true if it accepts; false otherwise
+     */
+    boolean accept(String text);
+}
